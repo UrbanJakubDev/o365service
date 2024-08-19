af4cedc07dea99f8c9cb2ed983736bc749ca7655
UrbanJakubDev <jakub.73@hotmail.com>
2024-08-19 12:45:01 +0200

    chore: Update package name to "o365_service" in setup.py and fix import statement in run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import os
 from dotenv import load_dotenv
 
-from o365.o365 import SharepointService
+from o365_service.o365 import SharepointService
 
 
 load_dotenv()
--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 setup(
     name="o365_service",
     version="1.0.0",
+    packages=find_packages(include=['o365', 'o365.*']),
     description="custom o365 service for download files from sharepoint drive",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     author="Jakub Urban",
     author_email="urbanjakubdev@gmail.com",
     url="https://github.com/UrbanJakubDev/o365service",
-    packages=find_packages(),
     install_requires=[
         # Add your dependencies here
         # Example: "numpy >= 1.18.0"
