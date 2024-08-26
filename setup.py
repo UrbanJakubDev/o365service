2ffc32f14110a8f45a138847839f2dd60e2823e4
UrbanJakubDev <jakub.73@hotmail.com>
2024-08-26 09:49:07 +0200

    chore: Adjust package name and import statement in setup.py
    The package name in setup.py was adjusted to "o365Service" and the import statement in run.py was fixed accordingly. This ensures consistency and proper functionality of the o365Service package.

--- setup.py
+++ setup.py
@@ -5,18 +5,16 @@ from setuptools import setup, find_packages
 setup(
     name="o365Service",
     version="1.0.1",
-    packages=find_packages(include=['o365', 'o365.*']),
-    description="custom o365 service for download files from sharepoint drive",
+    packages=find_packages(include=['o365Service', 'o365Service.*']),  # Adjusted to find the correct package
+    description="Custom o365 service for downloading files from SharePoint drive",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     author="Jakub Urban",
     author_email="urbanjakubdev@gmail.com",
     url="https://github.com/UrbanJakubDev/o365service",
     install_requires=[
-        # Add your dependencies here
-        # Example: "numpy >= 1.18.0"
         "msal >= 1.30.0",
-         "requests >= 2.32.3",
+        "requests >= 2.32.3",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
